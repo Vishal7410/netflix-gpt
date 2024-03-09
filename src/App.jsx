a0c2805930e703ef7aa0208ab789355a991d42b0
Vishal7410 <vishalpardhi2001@gmail.com>
2024-03-10 00:38:00 +0530

    sign in/sign up form && login/header

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Body from './Components/Body'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <div>
-
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </div>
-      
-    </>
-  )
+const App = () => {
+  return <Body/>
 }
 
 export default App
