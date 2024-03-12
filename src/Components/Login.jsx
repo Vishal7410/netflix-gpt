55b9603349cc373a51bee8072557bba4a95c304e
Vishal7410 <vishalpardhi2001@gmail.com>
2024-03-12 23:23:21 +0530

    validation deploy in firebase done

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,58 +1,98 @@
-import Header from './Header'
-import Logo from '../assets/home-bg.jpg'
-import { useState } from 'react'
- 
+import { checkValidateData } from "../utils/validate";
+import Header from "./Header";
+import { useRef, useState } from "react";
 
 const Login = () => {
-    const [isSignInForm, setisSignInForm] = useState(true)
-    const toggleSignUpForm = ()=> {
-        setisSignInForm(!isSignInForm);
-    }
+  const [isSignInForm, setisSignInForm] = useState(true);
+  const [errorMsg, seterrorMsg] = useState(null);
+  // const [name, setName] = useState(false)
+  
+  const name = useRef(null)
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const HandleButtonclick = () => {
+    // Validate the form data
+    //checkValidateData(email, password)
+
+    // console.log(email);
+    // console.log(password);
+    console.log(name.current.value);
+    console.log(email.current.value);
+    console.log(password.current.value);
+
+    const massage = checkValidateData(
+      
+      email.current.value,
+      password.current.value,
+      name.current.value,
+      // setName(!name.current.value)
+      
+    );
+    //  console.log(massage);
+    seterrorMsg(massage);
+  };
+
+  const toggleSignUpForm = () => {
+    setisSignInForm(!isSignInForm);
+  };
   return (
     <div>
-        <Header/>
-        <div className='absolute'>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
-         alt="Background Img "
-         />
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="Background Img "
+        />
+      </div>
 
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black  justify-center my-32 mx-auto right-0 left-0 text-white rounded-lg opacity-90 "
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
 
-        </div>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-6 w-full bg-gray-700 rounded-lg"
+          />
+        )}
 
-        <form type = "submit"
-         className= 'w-3/12 absolute p-12 bg-black  justify-center my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-90 '>
-            <h1 className='font-bold text-3xl py-4'>
-                {isSignInForm ? "Sign In" : "Sign Up"}
-                </h1>
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-6 w-full bg-gray-700 rounded-lg"
+        />
 
-         
-               {!isSignInForm && <input
-             type="text"
-             placeholder='Full Name'
-             className='p-4 my-6 w-full bg-gray-700 rounded-lg' 
-             />
-               }
-             
-            <input
-             type="text"
-             placeholder='Email Address'
-             className='p-4 my-6 w-full bg-gray-700 rounded-lg' 
-             />
+        <input
+          ref={password}
+          type="Password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <p className="text-red-700 font-bold text-lg py-2">{errorMsg}</p>
+        <button
+          className="p-4 my-6 w-full bg-red-700 rounded-lg"
+          onClick={HandleButtonclick}
+        >
+          {" "}
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
 
-            <input 
-            type="Password"
-            placeholder='Password'
-            className='p-4 my-4 w-full bg-gray-700 rounded-lg' 
-            />
-            <button className='p-4 my-6 w-full bg-red-700 rounded-lg'
-            > {isSignInForm ? "Sign In" : "Sign Up"}
-            </button>
-
-            <p className="py-4 cursor-pointer" onClick={toggleSignUpForm} >{isSignInForm? "New to Netflix? Sign Up Now" : "Alreadt registered? Sign In Now. "}</p>
-                
-        </form>
+        <p className="py-4 cursor-pointer" onClick={toggleSignUpForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Alreadt registered? Sign In Now. "}
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCk2fwEebOFdJB_UiIWUYCYIa1EYlfd8WI",
+  authDomain: "netflixgpt3-f7090.firebaseapp.com",
+  projectId: "netflixgpt3-f7090",
+  storageBucket: "netflixgpt3-f7090.appspot.com",
+  messagingSenderId: "748571071117",
+  appId: "1:748571071117:web:f9bec92dbb0baf07db48c4",
+  measurementId: "G-RJ8BW9BXB5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
