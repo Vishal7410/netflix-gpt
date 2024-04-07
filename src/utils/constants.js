da7290d7d996b1037778d7ff4c2376b8dac6d249
Vishal7410 <vishalpardhi2001@gmail.com>
2024-04-07 15:34:40 +0530

    fix bugs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+import { Processor } from "postcss";
+
 export const Logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const bgcImage = "https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg"
@@ -8,7 +11,8 @@ export const API_OPTION = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-       Authorization: "Bearer " +process.env.REACT_APP_TMDB_KEY 
+       Authorization: "Bearer "//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZjM1NGI2NTc5YzUxODUwYzIzMzczZjQwYmEyMTZlNCIsInN1YiI6IjY1ZjcyZGMwZDhmNDRlMDE2MzRmYjZiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F82euH840U9IGg3RtlJUPt8RQb2gXUHi-7981EcQtXQ
+       + import.meta.env.REACT_APP_TMDB_KEY 
     },
   };
 
@@ -23,4 +27,7 @@ export const API_OPTION = {
 // you get these API from the platform.openai.com // you get these API Keys. & you have to create your Api keyse 
 // keep it secreate
 
-export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY 
+
+ export const OPENAI_KEY = import.meta.env.REACT_APP_OPENAI_KEY
+
+
